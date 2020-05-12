1b6eaa752fe1395c0031f67800974fe6fa1f7ecb
Reychandra Atta <61732197+Retrivie@users.noreply.github.com>
2020-05-12 21:22:47 +0700

    Polymorphism PRINTER INHERITANCE

--- Program Printer I.cs
+++ Program Printer I.cs
@@ -1,42 +1,49 @@
-﻿using System;
+using System;
 
-namespace Printer_II
+namespace Printer
 {
-    public interface PrinterWindows
+    public class PrinterWindows
     {
-        void Show();
-        void Print();
+
+        public virtual void Show()
+        {
+            Console.WriteLine("Printer display dimension : x*y");
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine("Printer printer printing....");
+        }
     }
 
     public class Epson : PrinterWindows
     {
-        public void Show()
+        public override void Show()
         {
             Console.WriteLine("Epson display dimension : 10*11");
         }
-        public void Print()
+        public override void Print()
         {
             Console.WriteLine("Epson printer printing....");
         }
     }
     public class Canon : PrinterWindows
     {
-        public void Show()
+        public override void Show()
         {
             Console.WriteLine("Canon display dimension : 9.5*12");
         }
-        public void Print()
+        public override void Print()
         {
             Console.WriteLine("Canon printer printing....");
         }
     }
     public class LaserJet : PrinterWindows
     {
-        public void Show()
+        public override void Show()
         {
             Console.WriteLine("LaserJet display dimension : 12*12");
         }
-        public void Print()
+        public override void Print()
         {
             Console.WriteLine("LaserJet printer printing....");
         }
